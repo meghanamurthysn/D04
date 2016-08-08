fa5f4230dfb877b5bd5d661d0e8a1284640629e0
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-08 00:32:12 -0700

    HW04_ch08_ex05.py_v01

--- HW04_ch08_ex05.py
+++ HW04_ch08_ex05.py
@@ -2,3 +2,28 @@
 # See Chapter 8: Strings Exercise 5 for the task.
 # Please do provide function calls that test/demonstrate your
 # function.
+################################################################################
+def rot_letter(letter, n):
+	""" Rotate a letter by n places"""
+	if letter.islower():
+		return chr((ord(letter) - ord('a')+n) % 26 + ord('a'))
+	elif letter.isupper():
+		return chr((ord(letter) - ord('A')+n) % 26 + ord('A'))
+	else:
+		return letter
+
+def rot_word(word, n):
+	""" Rotate each letter in a word by n places"""
+	result_word = ''
+	for letter in word:
+		result_word += rot_letter(letter, n)
+	return result_word
+
+################################################################################
+def main():
+    print(rot_word('cheer', 7))
+    print(rot_word('melon', -10))
+    print(rot_word('IBM', -1))
+
+if __name__ == '__main__':
+    main()
